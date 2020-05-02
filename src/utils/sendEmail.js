a51800981b2aa61410b3ebd83e6e8deabd605826
unknown <liachenxiexu@gmail.com>
2020-05-03 00:24:04 +1000

    update email

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -1,26 +1,29 @@
 const nodemailer = require("nodemailer");
 
+const { EMAIL_HOST, EMAIL_PORT, EMAIL_ACCOUNT, EMAIL_PASSWORD } = process.env;
+
 async function sendEmail(user, booking) {
     let { email, firstName } = user;
     const { bookingDate } = booking;
+    // Test DATA
     email = "liachenxiexu@gmail.com";
     let transporter = nodemailer.createTransport({
-        host: "smtp.163.com",
-        port: 25,
+        host: EMAIL_HOST,
+        port: EMAIL_PORT,
         secure: false, // true for 465, false for other ports
         auth: {
-            user: "lia_chenxiexu@163.com",
-            pass: "LDGPOIDQMTHSXHNS"
-        }
+            user: EMAIL_ACCOUNT,
+            pass: EMAIL_PASSWORD
+        },
     });
 
     // send mail with defined transport object
     let info = await transporter.sendMail({
-        from: '"AIBT Team 👻" <lia_chenxiexu@163.com>', // sender address
+        from: `"AIBT Team 👻" <${EMAIL_ACCOUNT}>`, // sender address
         to: email, // list of receivers
-        subject: "Your Next AIBT Consultation Appointment", // Subject line
+        subject: 'Your Next AIBT Consultation Appointment', // Subject line
         text: `Hi ${firstName}, Just a friendly reminder that your next appointment at AIBT is coming up on .`, // plain text body
-        html: `<b>Hi ${firstName}, Just a friendly reminder that your next appointment at AIBT is coming up on .</b>` // html body
+        html: `<b>Hi ${firstName}, Just a friendly reminder that your next appointment at AIBT is coming up on .</b>`, // html body
     });
 
     console.log("Message sent: %s", info.messageId);
